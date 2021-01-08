c498892eb65b639a0bfc1dec3e1c1b79f18bdf5e
MDubovac <54212992+MDubovac@users.noreply.github.com>
2021-01-08 23:27:23 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,18 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "",
-    database = "pass_manager"
+    host = "HOST_NAME",
+    user = "USER_NAME",
+    password = "PASSWORD",
+    database = "DATABASE_NAME"
 )
 
 my_cursor = db.cursor()
+
+# In Order for this app to work, you will need to modify this file
+# You should enter valid data here instead of the placeholder
+# Example:
+#  host = "localhost"
+#  user = "root"
+#  password = "123456"
+#  database = "app_db"
